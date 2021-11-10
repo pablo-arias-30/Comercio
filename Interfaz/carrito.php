455ca9afc55a01ea0e802908f01aa41a40563bca
JAVIER DELGADO GARCÍA <Javier.Delgado6@alu.uclm.es>
2021-11-10 19:08:16 +0100

    Creación de carrito.php (no hace nada todavía)

---
+++ Interfaz/carrito.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset=utf-8 />
+    <link rel="stylesheet" href="stile.css" type="text/css">
+    <title>Catalogo Gafas de Sol</title>
+    <script src="model.js"></script>
+        <script src="agregar-articulo-presenter.js"></script>
+        <script>
+             let presenter;
+            function inicio() {
+           // presenter.init();
+           presenter = new AgregarArticuloPresenter(new ComprasApp(), document); //document porque el id esta dentro y solo nos interesa el id de la tarea para mostrar el mensaje de que se ha borrado
+                console.log(presenter.model);
+                presenter.refresh();
+
+            }
+        </script>
+
+</head>
+
+<body onload="inicio()">
+
+    <a href="index.html"><img id="img1" src="../Recursos/VISUALVISION.png"></a>
+    <a href="carrito.php"><img id="compra" src="../Recursos/carrito.png"></a>
+    <label id="cesta" width=2px height=2px>0</label>
+    <a href="miperfil.php"><img id="usuario" src="../Recursos/usuario.png"></a>
+    <br>
+    <ul class="Menu">
+        <li><a href="index.html">PÁGINA PRINCIPAL</a></li>
+        <li><a href="">CATÁLOGO</a>
+            <ul>
+                <li><a href="catalogo-gafasDeSol.php">Gafas de sol</a></li>
+                <li><a href="catalogo-gafasDeVista.php">Gafas de vista</a></li>
+                <li><a href="catalogo-otros.html">Otros</a></li>
+            </ul>
+        </li>
+        <li><a href="miperfil.php">MI CUENTA</a>
+            <ul>
+                <li><a href="iniciarSesion.html">Iniciar sesión</a></li>
+                <li><a href="registro.html">Nuevo usuario</a></li>
+            </ul>
+        </li>
+        <li> <a href="pedirCita.php">PEDIR CITA</a></li>
+
+    </ul>
+
+    </div>
+
+
+</body>
+
+</html>
