b04b4004a6fe0cb692eb7ae0943c53cbb12bf9da
PABLO ARIAS ÚBEDA <Pablo.Arias1@alu.uclm.es>
2021-11-13 19:24:06 +0100

    Mensaje carrito vacío

--- Interfaz/carrito-presenter.js
+++ Interfaz/carrito-presenter.js
@@ -1,7 +1,7 @@
 class CarritoPresenter {
     constructor(model, view) {
         this.model = model;
-        this.view = view; 
+        this.view = view;
         // es igual al document
     }
 
@@ -9,19 +9,26 @@ class CarritoPresenter {
 
     refresh() {
         console.log(this.model);
-        for (let articulo of this.model.compras) {
-            console.log(articulo);
+        if (this.model.compras.length == 0) {
             let bloqueGrande = document.getElementById('bloqueGrande');
-            let bloquePequeño = document.createElement('div');
-            bloquePequeño.innerHTML = '<div id="gafas"><img id="imagen" src="../Recursos/'+articulo._imagen+'"></img>'
-            +'<img id="marca" src="../Recursos/'+articulo._logo+'"></img>'+
-            '<p id="p1"><strong>'+articulo._nombre.replace(/%20/g, " ")+'</strong></p><p id="p1">Color:'+articulo._color+'</p>'+
-            '<p id="p1"><strong>'+articulo._precio+' €</strong></p>'+
-            '<p id="p1">Cantidad seleccionada: '+articulo._cantidad+'</p>'+
-            '<p id="p1">ID de Referencia:'+articulo._id+'</p></div>';
-            //Añadimos precio, color, cantidad, imagen, etc. Replace nos permite reemplazar los %20 de los espacios
-            bloqueGrande.appendChild(bloquePequeño);
+            let h2 = document.createElement('h2');
+            h2.innerHTML = '<h2>¡La cesta de la compra está vacía!</h2>';
+            bloqueGrande.appendChild(h2);
+        } else {
+            for (let articulo of this.model.compras) {
+                console.log(articulo);
+                let bloqueGrande = document.getElementById('bloqueGrande');
+                let bloquePequeño = document.createElement('div');
+                bloquePequeño.innerHTML = '<div id="gafas"><img id="imagen" src="../Recursos/' + articulo._imagen + '"></img>'
+                    + '<img id="marca" src="../Recursos/' + articulo._logo + '"></img>' +
+                    '<p id="p1"><strong>' + articulo._nombre.replace(/%20/g, " ") + '</strong></p><p id="p1">Color:' + articulo._color + '</p>' +
+                    '<p id="p1"><strong>' + articulo._precio + ' €</strong></p>' +
+                    '<p id="p1">Cantidad seleccionada: ' + articulo._cantidad + '</p>' +
+                    '<p id="p1">ID de Referencia:' + articulo._id + '</p></div>';
+                //Añadimos precio, color, cantidad, imagen, etc. Replace nos permite reemplazar los %20 de los espacios
+                bloqueGrande.appendChild(bloquePequeño);
 
+            }
         }
     }
 }
